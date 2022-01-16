c00102c2ea0d442f17004764df17fbacfa03f6f3
Jairo Pinheiro <jairopnhr@gmail.com>
2022-01-15 21:26:18 -0300

    Meu Pegueno projeto

---
+++ src/main/java/com/example/cadastrocli/CadastroCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastrocli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadastrocli/CadastroCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastrocli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
